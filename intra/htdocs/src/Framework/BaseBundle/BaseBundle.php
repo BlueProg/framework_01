1795833331d3bf1aafccced236f8cf1283664063
BlueProg <blueprog42@gmail.com>
2015-03-24 19:20:16 +0100

    del

--- intra/htdocs/src/Framework/BaseBundle/BaseBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Framework\BaseBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class BaseBundle extends Bundle
-{
-}
--- intra/htdocs/src/Framework/UserBundle/FrameworkUserBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Framework\UserBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class FrameworkUserBundle extends Bundle
-{
-}
