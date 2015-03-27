1ae7b89524118fe3d5a374a8f7c8fedc80e68fc3
BlueProg <blueprog42@gmail.com>
2015-03-27 21:05:05 +0100

    maj framework1

--- htdocs/src/Framework/InterfaceBundle/Controller/DefaultController.php
+++ htdocs/src/Framework/InterfaceBundle/Controller/DefaultController.php
@@ -3,11 +3,24 @@
 namespace Framework\InterfaceBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('FrameworkInterfaceBundle:Default:index.html.twig');
+        $mode_connect = "";
+        $securityContext = $this->container->get('security.context');
+        if ($securityContext->isGranted('ROLE_ADMIN')) {
+            $mode_connect = "Admin";
+        }
+        else if ($securityContext->isGranted('ROLE_USER')) {
+            $mode_connect = "User";
+        }
+        else
+            $mode_connect = "Anonyme";
+        return $this->render('FrameworkInterfaceBundle:Default:index.html.twig', array(
+            'mode_connect' => $mode_connect
+        ));
     }
 }
