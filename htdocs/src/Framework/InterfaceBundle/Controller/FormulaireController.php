ad3a777be6732f9e2c68b268e08505155cb3e287
BlueProg <blueprog42@gmail.com>
2015-04-01 20:32:35 +0200

    maj form+ mail

--- htdocs/src/Framework/InterfaceBundle/Controller/FormulaireController.php
+++ htdocs/src/Framework/InterfaceBundle/Controller/FormulaireController.php
@@ -4,11 +4,49 @@ namespace Framework\InterfaceBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class FormulaireController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('FrameworkInterfaceBundle:Formulaire:index.html.twig');
+        $data  = array();
+
+        $form = $this->createFormBuilder($data)
+            ->add('title_field', 'text')
+            ->add('name_field', 'text')
+            ->add('email_field', 'email')
+            ->add('query', 'textarea')
+            ->getForm();
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+            $data = $form->getData();
+            echo "<pre>";
+            var_dump($data);
+            echo "</pre>";
+            $message = \Swift_Message::newInstance()
+                ->setSubject($data['title_field'])
+                ->setFrom($data['blueprog42@gmail.com'])
+                ->setTo('blueprog42@gmail.com')
+                ->setBody(
+                    $this->renderView(
+                        'FrameworkInterfaceBundle:Formulaire:index.html.twig', array
+                        ('form' => $form->createView())
+                    )
+                );
+            $this->get('mailer')->send($message);
+
+            //var_dump($data);
+            return new RedirectResponse($this->generateUrl('framework_interface_homepage'));;
+        }
+
+        return $this->render('FrameworkInterfaceBundle:Formulaire:index.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
+//    public function indexAction()
+//    {
+//        return $this->render('FrameworkInterfaceBundle:Formulaire:index.html.twig');
+//    }508875848
 }
---
+++ htdocs/web/js/script.js
@@ -0,0 +1,8 @@
+/**
+ * Created by mmole on 31/03/15.
+ */
+function checkmail(mailteste)
+{
+    console.log("passe");
+
+}
